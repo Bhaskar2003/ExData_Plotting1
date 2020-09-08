bb39b1bd598a5872d7b2eb911ea83ef7d818cecc
Bhaskar Sharma <66830739+Bhaskar2003@users.noreply.github.com>
2020-09-08 17:01:36 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,3 +1,5 @@
+#All the files already downloaded in the directory
+
 power<-read.table("household_power_consumption.txt",header=TRUE, sep=";")
 required<- subset(power, Date=="1/2/2007" | Date=="2/2/2007")
 required$Global_active_power=as.numeric(required$Global_active_power)    #converting into numeric from character
